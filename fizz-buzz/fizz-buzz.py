7e116be8424fb02f2ac2e9312bb37ced758ac022
Abhishek Mathur <mathur17021play@gmail.com>
2021-10-13 16:37:46 +0530

    Time: 32 ms (99.20%), Space: 15.1 MB (67.33%) - LeetHub

--- fizz-buzz/fizz-buzz.py
+++ fizz-buzz/fizz-buzz.py
@@ -1,14 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         ans = []
-        for i in range(0,n):
-            ind = i+1
-            if ind%3==0 and ind%5==0:
-                ans.append('FizzBuzz')
-            elif ind%3==0:
-                ans.append('Fizz')
-            elif ind%5==0:
-                ans.append('Buzz')
-            else:
-                ans.append(str(ind))
+        for i in range(1,n+1):
+            temp=""
+            if not i%3:
+                temp+=('Fizz')
+            if not i%5:
+                temp+=('Buzz')
+            if i%3 and i%5:
+                temp+=(str(i))
+            ans.append(temp)
         return ans

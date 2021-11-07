c02a75b2dc21308e8dcdb657d26ab789fc94665d
Abhishek Mathur <mathur17021play@gmail.com>
2021-11-07 12:26:43 +0530

    Time: 32 ms (89.57%), Space: 14.5 MB (10.58%) - LeetHub

--- multiply-strings/multiply-strings.py
+++ multiply-strings/multiply-strings.py
@@ -2,6 +2,7 @@
 """
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
         if num1=="0" or num2=="0":
             return "0"
         def sumLists(l):
